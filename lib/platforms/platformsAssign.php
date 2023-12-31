4e6a5d086de47007c766f4ab7ccd5223d910ab40
francisco mancardi <francisco.mancardi@tesisquare.com>
2023-12-31 16:29:24 +0100

    typo error

--- lib/platforms/platformsAssign.php
+++ lib/platforms/platformsAssign.php
@@ -6,42 +6,29 @@
  * Platform link/unlink from a test plan
  * 
  * @package     TestLink
- * @author      eloff
- * @copyright   2005-2022, TestLink community 
+ * @copyright   2005-2020, TestLink community 
  * @filesource  platformsAssign.php
  * @link        http://www.testlink.org
- *
  *
  **/
 require_once("../../config.inc.php");
 require_once("common.php");
 require_once("opt_transfer.php");
-testlinkInitPage($db,false,false,"checkRights");
+testlinkInitPage($db);
 
 $templateCfg = templateConfiguration();
 
 $opt_cfg = opt_transf_empty_cfg();
 $opt_cfg->js_ot_name = 'ot';
 $args = init_args($opt_cfg);
-
-if ($args->edit == 'testproject') {
-  show_instructions('platformAssign');
-  exit();
-}
-
-
-$smarty = new TLSmarty();
 $tplan_mgr = new testplan($db);
 $platform_mgr = new tlPlatform($db, $args->tproject_id);
+checkPageAccess($db,$args);  // Will exit if check failed
+$gui = initializeGui($db,$args,$platform_mgr); 
 
-$gui = new stdClass();
-$gui->platform_assignment_subtitle = null;
-$gui->tplan_id = $args->tplan_id;
-$gui->can_do = isset($args->tplan_id);
-$gui->mainTitle = lang_get('add_remove_platforms');
-$gui->warning = '';
+$smarty = new TLSmarty();
 
-if (isset($args->tplan_id)) {
+if (isset($args->tplan_id) && $args->tplan_id >0) {
   // do following check to give warning to user
   // if test plan has test case versions with platform_id=0
   // this means that right now there are not platforms linked to test plan.
@@ -68,18 +55,59 @@ if (isset($args->tplan_id)) {
     $gui->mainTitle = sprintf($gui->mainTitle,$tplanData['name']);
   }
 
-    
-  if ($args->doAction == 'doAssignPlatforms') {
+  // -------------------------------------------------------------------------------------------------
+  if ($args->doAction == 'doAssignPlatforms' || $args->doAction == 'doAssignAndLinkTCV') {
     $platform_mgr->linkToTestplan($args->platformsToAdd,$args->tplan_id);
     $platform_mgr->unlinkFromTestplan($args->platformsToRemove,$args->tplan_id);
-    if( $fix_needed && count($args->platformsToAdd) == 1)
-    {
+  }	
+
+  if ($args->doAction == 'doAssignPlatforms') {
+    if( $fix_needed && count($args->platformsToAdd) == 1) {
       reset($args->platformsToAdd);
       $tplan_mgr->changeLinkedTCVersionsPlatform($args->tplan_id,0,current($args->platformsToAdd));
     }
     // Update option panes with newly updated config
     $gui->platform_count_js = init_option_panels($tplan_mgr, $platform_mgr, $opt_cfg, $args);
   }
+
+  if ($args->doAction == 'doAssignAndLinkTCV') {
+
+    // Get all linked platforms and filter out the platforms WITH TESTCASES
+     $platWithZeroTCV = [];
+     if ($args->onRightSide != null && count($args->onRightSide)) {
+      foreach($args->onRightSide as $platID) {
+        if (!isset($qtyByPlatform[$platID])) {
+          $platWithZeroTCV[$platID] = $platID;  
+        }
+      }
+     }
+
+    // get all linked to the first of selected platforms
+    $fromPlat = intval($_REQUEST["to_select_box"]);
+    if ($fromPlat == 0) {
+      // try to get first on set
+      if ($gui->hasTCVWithPlat) {
+        $platSetLink = array_keys($qtyByPlatform);
+        $fromPlat = current($platSetLink);
+      }	
+    }
+
+    // test again
+    if ($fromPlat != 0) {
+       // does this platform linked TCV? if not -> nothing can be done
+       if (isset($qtyByPlatform[$fromPlat])) {
+         foreach($platWithZeroTCV as $toPlat) {
+           $tplan_mgr->copyLinkFromPlatformToPlatform($args->tplan_id,$fromPlat,$toPlat,$args->user_id);
+         }	
+       }
+      // Update option panes with newly updated config
+      $gui->platform_count_js = init_option_panels($tplan_mgr, $platform_mgr, $opt_cfg, $args);
+    }
+  }
+  // -------------------------------------------------------------------------------------------------
+
+
+
 }
 
 
@@ -103,14 +131,6 @@ function init_option_panels(&$tplan_mgr, &$platform_mgr,
                             &$opt_cfg, &$args)
 {
 
-  /*
-  $opx = [
-    'enable_on_design' => false, 
-    'enable_on_execution' => true,
-    'is_open' => true
-  ];
-  */
-  
   $opt_cfg->from->map = $platform_mgr->getAllAsMap(config_get('platforms')->allowedOnAssign);
 
   $optLTT = null;
@@ -146,36 +166,64 @@ function init_args(&$opt_cfg)
   $added = $opt_cfg->js_ot_name . "_addedRight";
   $removed = $opt_cfg->js_ot_name . "_removedRight";
 
-  $iParams = array( "tplan_id" => array(tlInputParameter::INT_N),
-                    "edit" => array(tlInputParameter::STRING_N,0,100),
-                    "doAction" => array(tlInputParameter::STRING_N,0,20),
-                    $added => array(tlInputParameter::STRING_N),
-                    $removed => array(tlInputParameter::STRING_N));
+  $iParams = [
+    "tproject_id" => [tlInputParameter::INT_N],
+    "tplan_id" => [tlInputParameter::INT_N],
+    "edit" => [tlInputParameter::STRING_N,0,100],
+    "doAction" => [tlInputParameter::STRING_N,0,20],
+    $added => [tlInputParameter::STRING_N],
+    $removed => [tlInputParameter::STRING_N]
+  ];
 
   $pParams = R_PARAMS($iParams);
 
   $args = new stdClass();
+  $args->tproject_id = $pParams["tproject_id"];
   $args->tplan_id = $pParams["tplan_id"];
-  $args->platformsToAdd = null;
-  $args->platformsToRemove = null;
   $args->edit = $pParams["edit"];
   $args->doAction = $pParams["doAction"];
-  $args->tproject_id = isset($_SESSION['testprojectID']) ? $_SESSION['testprojectID'] : 0;
+  $args->currentUser = $_SESSION['currentUser'];
   
+  $args->platformsToAdd = [];
   if ($pParams[$added] != "") {
-     $args->platformsToAdd = explode(",", $pParams[$added]);
+     $args->platformsToAdd = (array)explode(",", $pParams[$added]);
   }
   
+  $args->platformsToRemove = [];
   if( $pParams[$removed] != "" ) 
   {
-    $args->platformsToRemove = explode(",", $pParams[$removed]);
+    $args->platformsToRemove = (array)explode(",", $pParams[$removed]);
   }
 
   return $args;
 }
 
 
-function checkRights(&$db,&$user)
+/**
+ * 
+ */
+function initializeGui(&$dbH,&$argsObj,&$platformMgr) 
 {
-  return $user->hasRightOnProj($db,'testplan_add_remove_platforms');
+  $gui = $platformMgr->initViewGui($argsObj->currentUser,$argsObj);
+  $gui->activeMenu['projects'] = 'inactive'; // done in initViewGui
+  $gui->activeMenu['plans'] = 'active';
+
+  $gui->platform_assignment_subtitle = null;
+  $gui->warning = '';
+  $gui->tplan_id = $argsObj->tplan_id;
+  $gui->tproject_id = $argsObj->tproject_id;
+  $gui->can_do = isset($argsObj->tplan_id);
+  $gui->mainTitle = lang_get('add_remove_platforms');
+
+  return $gui;
+}
+
+/**
+ *
+ */
+function checkPageAccess(&$db,&$argsObj) {
+  $env['script'] = basename(__FILE__);
+  $env['tproject_id'] = isset($argsObj->tproject_id) ? $argsObj->tproject_id : 0;
+  $env['tplan_id'] = isset($argsObj->tplan_id) ? $argsObj->tplan_id : 0;
+  $argsObj->currentUser->checkGUISecurityClearance($db,$env,array('testplan_add_remove_platforms'),'and');
 }
