fa80686814e523c4519f5d4c2a13195a8023dc23
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2026-08-16 08:29:13 +0300

    fix: strict type check in MD5->bcrypt migration + Jira Cloud accountId support
    - lib/functions/tlUser.class.php: strlen() === 32 (type juggling security)
    - lib/issuetrackerintegration/jirarestInterface.class.php: look up reporter
      accountId via email, fallback to id (port of testlink_1_9 df6ee30ba)
    - third_party/fayp-jira-rest/Jira.php: add getAccountId() method (upstream
      exception bug fixed: $result->errorMessages instead of undefined $items)

--- lib/issuetrackerintegration/jirarestInterface.class.php
+++ lib/issuetrackerintegration/jirarestInterface.class.php
@@ -393,8 +393,13 @@ class jirarestInterface extends issueTrackerInterface
           // After Atlassian GDRP Changes
           // $issue['fields']['reporter'] = 
           //  array('name' => (string)$opt->reporter);
-          $issue['fields']['reporter'] = 
-            array('id' => (string)$opt->reporter);
+          $accountid = $this->APIClient->getAccountId($opt->reporter_email);
+          if ($accountid) {
+            $issue['fields']['reporter'] = array('accountId' => (string)$accountid);
+          } else {
+            $issue['fields']['reporter'] = 
+              array('id' => (string)$opt->reporter);
+          }
         }
 
         if (property_exists($opt, 'issueType')) {
